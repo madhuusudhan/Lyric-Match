1beb0fdd1fed78ba2c370fa63108eb2c20546dbe
Madhusudhan <144603165+madhuusudhan@users.noreply.github.com>
2025-03-14 21:13:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://lyric-match-g3x0.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
